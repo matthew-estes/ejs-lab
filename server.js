af1c80a7b34a567c41d896182814d5a4a081d771
matthew-estes <matthew.estes19@gmail.com>
2024-06-04 09:24:59 -0400

    create partials directory; create nav.ejs within it

--- server.js
+++ server.js
@@ -1,60 +1,68 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.render('home.ejs');
+app.get("/", function(req, res) {
+  res.render("home.ejs");
 });
 
 const RESTAURANT = {
-  name: 'The Green Byte Bistro',
+  name: "The Green Byte Bistro",
   isOpen: true,
-  address: '742 Evergreen Rd, Mapleview, OS 45502',
-  phone: '555-321-9876',
+  address: "742 Evergreen Rd, Mapleview, OS 45502",
+  phone: "555-321-9876",
   menu: [
-    { 
+    {
       id: 1,
-      name: 'Quantum Quinoa Mushroom Burger',
-      price: 13.00,
+      name: "Quantum Quinoa Mushroom Burger",
+      price: 13.0,
       rating: 4,
-      category: 'mains',
-      details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+      category: "mains",
+      details:
+        "A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.",
     },
-    { 
+    {
       id: 2,
-      name: 'Binary Berry Cheesecake',
+      name: "Binary Berry Cheesecake",
       price: 10.11,
       rating: 3,
-      category: 'desserts',
-      details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+      category: "desserts",
+      details:
+        "A creamy cheesecake bursting with flavor. A mix of berries in every byte.",
     },
-    { 
+    {
       id: 3,
-      name: 'Recursive Rigatoni',
-      price: 17.00,
+      name: "Recursive Rigatoni",
+      price: 17.0,
       rating: 5,
-      category: 'mains',
-      details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+      category: "mains",
+      details:
+        "A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You'll keep coming back for more.",
     },
-    { 
+    {
       id: 4,
-      name: 'Pumpkin Pi Squared',
+      name: "Pumpkin Pi Squared",
       price: 3.14,
       rating: 5,
-      category: 'desserts',
-      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+      category: "desserts",
+      details:
+        "A delightful pumpkin dessert, squared and spiced to perfection.",
     },
-    { 
+    {
       id: 5,
-      name: 'Fibonacci String Bean Fries',
+      name: "Fibonacci String Bean Fries",
       price: 11.23,
       rating: 5,
-      category: 'sides',
-      details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
-    }
-  ]
-}
+      category: "sides",
+      details:
+        "Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.",
+    },
+  ],
+};
+
+
+
+
+
 
 
 app.listen(3000);
-
